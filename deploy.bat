8a15a683a28307ca4ebec0ddef68d5d4f14a0247
Vincent Ducamps <vincent@ducamps.win>
2021-11-25 15:57:36 +0100

    fix deploy

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 set appname=monappli-web
 SET src=%~dp0%appname%\target\%appname%.war
 SET dest=C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps
-copy %src% "%dest%"
+copy "%src%" "%dest%"
